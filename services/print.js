203131653cb8997edb3262847982f3d36a6272c1
Deyweson <devdeyve@gmail.com>
2024-06-14 17:46:29 -0300

    Desenvolvido a funcionalidade de juntar as imagens gerando um video mp4

--- services/print.js
+++ services/print.js
@@ -7,10 +7,10 @@ let count = 0
 
 function takePrint() {
     if (is_printing) {
-        count++
+        
 
         const filePath = path.join(__dirname, 'imgs', `screenshot${count}.jpg`);
-
+        count++
         const dir = path.dirname(filePath);
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
@@ -24,14 +24,14 @@ function takePrint() {
 }
 const stopPrint = () => {
     is_printing = false
-    count = 0
 }
 function startPrint() {
+    count = 0
     is_printing = true
 }
 
 
-setInterval(takePrint, 5000)
+setInterval(takePrint, 2000)
 
 const startBtn = document.querySelector('#startbtn')
 const stopBtn = document.querySelector('#stopbtn')
@@ -40,4 +40,63 @@ startBtn.addEventListener('click', () => {
 })
 stopBtn.addEventListener('click', () => {
     stopPrint()
+})
+
+
+
+
+
+
+const filePath = path.join(__dirname, 'imgs', `screenshot${count}.jpg`);
+
+const dir = path.dirname(filePath);
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
+
+var videoshow = require('videoshow')
+
+
+
+
+var videoOptions = {
+    fps: 25,
+    loop: 1, // seconds
+    transition: false,
+    transitionDuration: 0, // seconds
+    videoBitrate: 1024,
+    videoCodec: 'libx264',
+    size: '640x?',
+    audioBitrate: '128k',
+    audioChannels: 2,
+    format: 'mp4',
+    pixelFormat: 'yuv420p'
+}
+
+function makeVideo(imagems) {
+    videoshow(imagems, videoOptions)
+        .save('video.mp4')
+        .on('start', function (command) {
+            console.log('ffmpeg process started:', command)
+        })
+        .on('error', function (err, stdout, stderr) {
+            console.error('Error:', err)
+            console.error('ffmpeg stderr:', stderr)
+        })
+        .on('end', function (output) {
+            console.error('Video created in:', output)
+        })
+}
+
+const videoBtn = document.querySelector('#videobtn')
+videoBtn.addEventListener('click', () => {
+    var imagems = []
+    console.log(count)
+    for (let i = 0; i < count; i++) {
+        let path = `./imgs/screenshot${i}.jpg`
+        console.log(path)
+        imagems.push(path)
+    }
+
+    makeVideo(imagems)
 })
