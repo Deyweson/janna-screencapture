8e2ac814619ecf588bc746a78ea2543bd8ec0c1c
Deyweson <devdeyve@gmail.com>
2024-06-15 18:13:17 -0300

    Ajustes no css e reorganizado as pastas do projeto

--- electron/main.js
+++ electron/main.js
@@ -3,11 +3,12 @@ const { app, Tray, Menu, nativeImage, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 300,
-        height: 450,
+        height: 550,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-        }
+        },
+        resizable: false,
     })
 
     win.loadFile('index.html')
